b2a0b16b45da33b6c79c5ec3a1c2e9f6192693af
T-Yeon <thompson_yeon@bah.com>
2022-12-07 15:16:11 -0600

    initial commit. Added POJOs Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgpetclinic.model;
+
+
+import java.time.LocalDate;
+
+public class Pet {
+    
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+
+
+    public PetType getPetType() {
+        return this.petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return this.birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+
+    
+}
